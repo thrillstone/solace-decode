182991243c18409ec2960c8fbac3bf775aa28046
Justin Su <justisu@amazon.com>
2021-03-04 20:08:45 -0500

    Add fuzzy matching for /search/messages

--- src/main/java/com/example/solace/decode/repository/es/ESMessageRepository.java
+++ src/main/java/com/example/solace/decode/repository/es/ESMessageRepository.java
@@ -1,11 +1,35 @@
 package com.example.solace.decode.repository.es;
 
 import com.example.solace.decode.model.es.ESMessage;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
 
 public interface ESMessageRepository extends ElasticsearchRepository<ESMessage, String> {
+    /*
+{
+	"bool": {
+		"must": {
+			"fuzzy": {
+				"payload": {
+					"value": "?0"
+				}
+			}
+		}
+	}
+}
+     */
+    @Query("{\n" +
+            "\t\"bool\": {\n" +
+            "\t\t\"must\": {\n" +
+            "\t\t\t\"fuzzy\": {\n" +
+            "\t\t\t\t\"payload\": {\n" +
+            "\t\t\t\t\t\"value\": \"?0\"\n" +
+            "\t\t\t\t}\n" +
+            "\t\t\t}\n" +
+            "\t\t}\n" +
+            "\t}\n" +
+            "}")
     List<ESMessage> findAllByPayloadContains(String payload);
 }
