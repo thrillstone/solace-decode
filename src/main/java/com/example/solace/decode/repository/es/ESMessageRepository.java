7b5575db0380bf6efb5de7370387fce1dc3a1255
Jeffrey Douangpaseuth <jdouangpaseuth@hotmail.ca>
2021-03-05 00:21:33 -0500

    fix multi-word searches

--- src/main/java/com/example/solace/decode/repository/es/ESMessageRepository.java
+++ src/main/java/com/example/solace/decode/repository/es/ESMessageRepository.java
@@ -3,11 +3,29 @@ package com.example.solace.decode.repository.es;
 import com.example.solace.decode.model.es.ESMessage;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.data.elasticsearch.annotations.Query;
 
 import java.util.List;
 
 public interface ESMessageRepository extends ElasticsearchRepository<ESMessage, String> {
-    @Query("{\"bool\":{\"must\":{\"fuzzy\":{\"payload\":{\"value\":\"?0\"}}},\"should\":[{\"rank_feature\":{\"field\":\"search_clicks\"}}]}}")
+    @Query("{" +
+            "\"bool\": {" +
+                "\"must\": {" +
+                    "\"multi_match\": {" +
+                        "\"fields\": [" +
+                            "\"payload\"" +
+                        "]," +
+                        "\"query\": \"?0\"," +
+                        "\"fuzziness\": \"AUTO\"" +
+                    "}" +
+                "}," +
+                "\"should\": [" +
+                    "{" +
+                        "\"rank_feature\": {" +
+                            "\"field\": \"search_clicks\"" +
+                        "}" +
+                    "}" +
+                "]" +
+            "}" +
+            "}")
     List<ESMessage> findAllByPayloadContains (String payload);
 }
--- src/main/java/com/example/solace/decode/rest/SearchController.java
+++ src/main/java/com/example/solace/decode/rest/SearchController.java
@@ -1,7 +1,9 @@
 package com.example.solace.decode.rest;
 
+import com.example.solace.decode.model.Message;
 import com.example.solace.decode.model.SearchRequest;
 import com.example.solace.decode.model.es.ESMessage;
+import com.example.solace.decode.repository.MessageRepository;
 import com.example.solace.decode.repository.es.ESMessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,16 +11,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class SearchController {
+    @Autowired
+    MessageRepository messageRepository;
+
     @Autowired
     ESMessageRepository ESRepo;
 
     @CrossOrigin
     @PostMapping("search/messages")
-    List<ESMessage> searchMessages(@RequestBody SearchRequest request) {
-        return ESRepo.findAllByPayloadContains(request.getContent());
+    List<Message> searchMessages(@RequestBody SearchRequest request) {
+        Iterable<String> ids = ESRepo.findAllByPayloadContains(request.getContent())
+                .stream()
+                .map(ESMessage::getId)
+                .collect(Collectors.toList());
+        Map<String, Message> savedMessages = new HashMap<>();
+        messageRepository.findAllById(ids).forEach(m -> savedMessages.put(m.getId(), m));
+        List<Message> actualList = new ArrayList<>(savedMessages.size());
+        ids.forEach(id -> actualList.add(savedMessages.get(id)));
+        return actualList;
     }
 }
